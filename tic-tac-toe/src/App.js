9f1595f40e042c3e4fc637f4a393bb7c6c82fd7a
Firdavs <firdavseminovhpl@gmail.com>
2023-01-02 21:55:00 +0500

    tictactoe

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Board from './TicTacToeGame/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <Board/>
+    </div>
+  );
+}
+
+export default App;
